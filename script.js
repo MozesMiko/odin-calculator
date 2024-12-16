3df24bee47ee0738c84f0ba85400c5792ae9c8e4
Mozes Miko <62065351+MozesMiko@users.noreply.github.com>
2024-12-16 11:01:38 +0100

    feat: add basic calculation logic for operators
    - Added calculation logic for addition (+), subtraction (-),
    multiplication (*), and division (/)
    - Implemented error handling for edge cases, such as division by zero
    - Ensured proper handling of intermediate results and operator precedence

--- script.js
+++ script.js
@@ -1,45 +1,112 @@
-let currentNumber = 4;
-let inputNumber = 6;
-let operator = "-";
+let displayNumber = "0";
+let currentNumber = null;
+let storedNumber = null;
+let operator = null;
+let result;
+
+const display = document.querySelector("#currentDisplay");
+display.innerText = displayNumber;
+
+const numbers = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
+
+const clearButton = document.querySelector("#btn-clear");
+const evaluateButton = document.querySelector("#btn-equals")
+
+for(let i = 0; i < numbers.length; i++) {
+  const numberButton = numbers[i];
+  numberButton.addEventListener("click", () => {
+    if(displayNumber == "0") {
+      displayNumber = "";
+      displayNumber += numberButton.value;
+      display.innerText = displayNumber;
+      currentNumber = Number(displayNumber);
+      printShit();
+    } else {
+      displayNumber += numberButton.value;
+      display.innerText = displayNumber;
+      currentNumber = Number(displayNumber);
+      printShit();
+    }
+  })
+}
+
+for(let i = 0; i < operators.length; i++) {
+  const operatorButton = operators[i];
+  operatorButton.addEventListener("click", () => {
+    if(storedNumber == null) {
+      storedNumber = currentNumber;
+      displayNumber = "";
+      operator = operatorButton.value;
+      currentNumber = null;
+      printShit();
+    } else {
+      displayNumber = "";
+      operate(storedNumber, operator, currentNumber);
+      storedNumber = result;
+      currentNumber = null;
+      display.innerText = storedNumber;
+      operator = operatorButton.value;
+      printShit();
+    }
+  })
+}
 
 const operate = function(num1, operator, num2) {
-  switch(operator) {
-    case "+":
-      console.log(num1 + num2);
-      break;
-    case "-":
-      console.log(num1 - num2);
-      break;
-    case "*":
-      console.log(num1 * num2);
-      break;
-    case "/":
-      console.log(num1 / num2);
-      break;
+  if(storedNumber != null && currentNumber != null) {
+    switch(operator) {
+      case "+":
+        result = (num1 + num2);
+        break;
+      case "-":
+        result = (num1 - num2);
+        break;
+      case "*":
+        result = (num1 * num2);
+        break;
+      case "/":
+        if(num2 === 0) {
+          result = ("Cannot divide by zero")
+        } else {
+          result = (num1 / num2);
+        }
+        break;
+    }
+    return result;
+  } else {
+    console.error("Cannor operate with null")
   }
 }
 
-const clearButton = document.querySelector("#clear");
+evaluateButton.addEventListener("click", () => {
+  operate(storedNumber, operator, currentNumber);
+  displayNumber = result.toString();
+  display.innerText = displayNumber;
+  // equation.innerText = storedNumber + " " + operator + " " + currentNumber + " =";
+  currentNumber = null;
+  storedNumber = result;
+  operator = null;
+  displayNumber = "";
+  printShit();
+})
 
-const clear = function() {
-  currentNumber = "";
-  inputNumber = "";
-  operator = "";
-  console.log(currentNumber);
-  console.log(inputNumber);
-  console.log(operator);
+clearButton.addEventListener("click", () => {
+  // var highlighted = document.getElementsByClassName("highlight");
+  currentNumber = 0;
+  displayNumber = "0";
+  storedNumber = null;
+  operator = null;
+  equation.innerText = "";
+  display.innerText = "0";
+  // while (highlighted.length > 0) {
+  //   highlighted[0].classList.remove("highlight");
+  // }
+})
+
+const printShit = function() {
+  // console.log("CurrentNumber is : " + currentNumber);
+  // console.log("StoredNumber is : " + storedNumber);
+  // console.log("displayNumber is : " + displayNumber);
+  console.log("Equation is: " + storedNumber + " " 
+              + operator + " " + currentNumber);
 }
-
-clearButton.addEventListener("click", clear)
-
-const addButton = document.querySelector("#add");
-const add = function(){
-  operator = "+";
-  console.log(operator);
-}
-
-addButton.addEventListener("click", add);
-
-console.log(currentNumber);
-console.log(inputNumber);
-console.log(operator);
